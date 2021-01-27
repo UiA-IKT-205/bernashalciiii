ff8325ec08d9e74f8d907a5ea28fbeaac64427b9
Berna Shalci <bernas@uia.no>
2021-01-27 10:41:00 +0100

    Added buttons

--- Pomodoro-main/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ Pomodoro-main/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -11,34 +11,110 @@ import no.uia.ikt205.pomodoro.util.millisecondsToDescriptiveTime
 
 class MainActivity : AppCompatActivity() {
 
+    // If you do not want to initialize a variable in the constructor,
+    // instead you want to initialize it later on and if you can guarantee,
+    // the initialization before using it.
     lateinit var timer:CountDownTimer
-    lateinit var startButton:Button
+    lateinit var startButton: Button
     lateinit var coutdownDisplay:TextView
 
-    val timeToCountDownInMs = 5000L
+    lateinit var button30: Button
+    lateinit var button60: Button
+    lateinit var button90: Button
+    lateinit var button120: Button
+    lateinit var stopButton: Button
+
+
+
+    var timeToCountDownInMs = 5000L
     val timeTicks = 1000L
+    var timeStopped = 0L
+    var isTimeStarted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       startButton = findViewById<Button>(R.id.startCountdownButton)
-       startButton.setOnClickListener(){
-           startCountDown(it)
-       }
-       coutdownDisplay = findViewById<TextView>(R.id.countDownView)
 
+        button30 = findViewById<Button>(R.id.startCountdownButton30)
+        button60 = findViewById<Button>(R.id.startCountdownButton60)
+        button90 = findViewById<Button>(R.id.startCountdownButton90)
+        button120 = findViewById<Button>(R.id.startCountdownButton120)
+
+        button30.setOnClickListener(){
+            if (!isTimeStarted){
+                timeToCountDownInMs = 30 * 60 * 1000;
+                updateCountDownDisplay(timeToCountDownInMs)
+            }
+
+
+        }
+        button60.setOnClickListener(){
+            if (!isTimeStarted){
+                timeToCountDownInMs = 60 * 60 * 1000;
+                updateCountDownDisplay(timeToCountDownInMs)
+            }
+
+        }
+        button90.setOnClickListener(){
+            if (!isTimeStarted){
+                timeToCountDownInMs = 90 * 60 * 1000;
+                updateCountDownDisplay(timeToCountDownInMs)
+            }
+
+        }
+        button120.setOnClickListener(){
+            if (!isTimeStarted){
+                timeToCountDownInMs = 120 * 60 * 1000;
+                updateCountDownDisplay(timeToCountDownInMs)
+            }
+
+        }
+
+        startButton = findViewById<Button>(R.id.startCountdownButton)
+        startButton.setOnClickListener(){
+            if (isTimeStarted){
+                Toast.makeText(this@MainActivity, "Timer running", Toast.LENGTH_SHORT).show()
+
+            }else {
+                startCountDown(it)
+
+                isTimeStarted = true;
+            }
+
+        }
+
+        coutdownDisplay = findViewById<TextView>(R.id.countDownView)
+
+        stopButton = findViewById<Button>(R.id.stopCountdownButton)
+        stopButton.setOnClickListener(){
+        }
+        if (isTimeStarted){
+            timer.cancel()
+            timeToCountDownInMs = timeStopped;
+            updateCountDownDisplay(timeToCountDownInMs)
+            isTimeStarted = false;
+
+        }else {
+            Toast.makeText(this@MainActivity, "Timer is not started!", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
+
 
     fun startCountDown(v: View){
 
         timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
             override fun onFinish() {
-                Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity,"Time done", Toast.LENGTH_SHORT).show()
+                updateCountDownDisplay(timeToCountDownInMs)
+                isTimeStarted = false
             }
 
             override fun onTick(millisUntilFinished: Long) {
-               updateCountDownDisplay(millisUntilFinished)
+                updateCountDownDisplay(millisUntilFinished)
+                timeToCountDownInMs = millisUntilFinished
             }
         }
 
